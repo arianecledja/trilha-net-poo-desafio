3e9ba56fa403d8dfac50617957124400d6f423dd
Ariane Santos <ariane225040@gmail.com>
2024-11-16 08:14:57 -0300

    realização de testes

--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
